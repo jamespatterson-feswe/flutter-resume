72e9a7c47bd1cd45a468d6bfa53b7ca83d628613
James Patterson <jamespatterson.feswe@yahoo.com>
2024-07-28 21:56:05 -0400

    build: finish building shell of appBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,28 +10,39 @@ class Demo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var james = Person('James Patterson', 'SWE');
-    
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white70,
         appBar: AppBar(
-          title: Center(
-            child: Text(
-              james.title()
-            )
-          ),
-        ),
+            title: Center(
+              child: Text(james.title()),
+            ),
+            backgroundColor: Colors.white70,
+            elevation: 0,
+            leading: IconButton(
+              icon: const Icon(Icons.menu),
+              tooltip: 'To Be Decided',
+              onPressed: () {},
+            ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.logout),
+                tooltip: 'To Be Decided',
+                onPressed: () {},
+              )
+            ]),
         body: Center(
           child: Container(
-            height: 300,
-            width: 300,
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(24),
-            ),
-            child: Text("the body")
-          ),
+              height: 300,
+              width: 300,
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: Text("the body")),
         ),
       ),
     );
@@ -52,7 +63,7 @@ class Person {
   ];
 
   String title() {
-    return '${(name != '' && occupation != '') ? '$name | $company | $occupation | ' : ''}Interactive Resume';
+    return '${(name != '' && occupation != '') ? '$name | $occupation | ' : ''}Interactive Resume';
   }
 
   Person(this.name, this.occupation);
