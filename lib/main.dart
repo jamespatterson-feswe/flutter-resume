615b5f90125486b053cec6fdabfdc6192edd1a80
James Patterson <jamespatterson.feswe@yahoo.com>
2024-07-29 01:06:55 -0400

    fix: changing to todos app, goo starting point

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,15 @@ class Demo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var james = Person('James Patterson', 'SWE');
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.yellow),
       home: Scaffold(
-        backgroundColor: Colors.white70,
-        appBar: AppBar(
-            title: Center(
-              child: Text(james.title()),
-            ),
-            backgroundColor: Colors.white70,
-            elevation: 0,
+          backgroundColor: Colors.yellow[200],
+          appBar: AppBar(
+            backgroundColor: Colors.yellow,
             leading: IconButton(
               icon: const Icon(Icons.menu),
-              tooltip: 'To Be Decided',
               onPressed: () {},
             ),
             actions: [
@@ -32,48 +26,14 @@ class Demo extends StatelessWidget {
                 tooltip: 'To Be Decided',
                 onPressed: () {},
               )
-            ]),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-                height: (MediaQuery.of(context).size.height / 3),
-                width: MediaQuery.of(context).size.width,
-                color: Colors.red),
-            Expanded(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                color: Colors.white
-              )
-            ),
-            Container(
-                height: (MediaQuery.of(context).size.height / 3),
-                width: MediaQuery.of(context).size.width,
-                color: Colors.blue
-            ),
-          ],
-        ),
-      ),
+            ],
+            bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(2.0),
+                child: Container(
+                  color: Colors.yellow[300],
+                  height: 2.0,
+                )),
+          )),
     );
   }
-}
-
-class Person {
-  var company = 'Branding Brand';
-  var name = '';
-  var occupation = '';
-
-  List<String> options = [
-    'main',
-    'download',
-    'experience',
-    'technology',
-    'information'
-  ];
-
-  String title() {
-    return '${(name != '' && occupation != '') ? '$name | $occupation | ' : ''}Interactive Resume';
-  }
-
-  Person(this.name, this.occupation);
 }
